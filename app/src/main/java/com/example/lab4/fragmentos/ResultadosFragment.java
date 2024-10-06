a76eb500ccd86f0b893ecbb001ba13881cfef56e
andres <a20191450@pucp.edu.pe>
2024-10-05 19:58:09 -0500

    fragmento resultado + sensor

--- app/src/main/java/com/example/lab4/fragmentos/ResultadosFragment.java
+++ app/src/main/java/com/example/lab4/fragmentos/ResultadosFragment.java
@@ -1,5 +1,12 @@
 package com.example.lab4.fragmentos;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -19,6 +26,7 @@ import com.example.lab4.recyclerview.Resultado;
 import com.example.lab4.recyclerview.ResultadosAdapter;
 import com.example.lab4.retrofit.ApiService;
 import com.example.lab4.retrofit.ResultadosResponse;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +37,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class ResultadosFragment extends Fragment {
+public class ResultadosFragment extends Fragment implements SensorEventListener {
 
     private RecyclerView recyclerView;
     private ResultadosAdapter adapter;
@@ -39,6 +47,12 @@ public class ResultadosFragment extends Fragment {
     private EditText editTextSeason;
     private EditText editTextRonda;
     private Button btnBuscar;
+
+    // Acelerómetro
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private static final float THRESHOLD = 20.0f;
+    private boolean isDialogShowing = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -72,6 +86,12 @@ public class ResultadosFragment extends Fragment {
             }
         });
 
+        // Inicializar el sensor
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager != null) {
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }
+
         return view;
     }
 
@@ -100,6 +120,8 @@ public class ResultadosFragment extends Fragment {
                 } else {
                     Toast.makeText(getContext(), "Uno de los datos ingresados de la competencia no es correcta.", Toast.LENGTH_LONG).show();
                 }
+
+                controlSensorState();
             }
 
             @Override
@@ -108,4 +130,71 @@ public class ResultadosFragment extends Fragment {
             }
         });
     }
+
+    // Controlar el estado del sensor
+    private void controlSensorState() {
+        if (resultadosList.isEmpty()) {
+            sensorManager.unregisterListener(this);
+        } else {
+            if (accelerometer != null) {
+                sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+            }
+        }
+    }
+
+    // Gestión del sensor
+    @Override
+    public void onResume() {
+        super.onResume();
+        controlSensorState();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Desregistrar el sensor
+        sensorManager.unregisterListener(this);
+        isDialogShowing = false;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float acceleration = (float) Math.sqrt(Math.pow(event.values[0], 2) + Math.pow(event.values[1], 2) + Math.pow(event.values[2], 2));
+            if (acceleration > THRESHOLD && !isDialogShowing) {
+                isDialogShowing = true;
+                mostrarDialogoConfirmacion();
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // No es necesario hacer nada aquí en este caso
+    }
+
+    private void mostrarDialogoConfirmacion() {
+        new MaterialAlertDialogBuilder(getContext())
+                .setTitle("Confirmación")
+                .setMessage("¿Desea limpiar los resultados?")
+                .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        resultadosList.clear();
+                        adapter.notifyDataSetChanged();
+                        editTextLigaId.setText("");
+                        editTextSeason.setText("");
+                        editTextRonda.setText("");
+                        isDialogShowing = false;
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        isDialogShowing = false;
+                    }
+                })
+                .show();
+    }
+
 }
